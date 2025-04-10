1eea33e70ae417a207faa16c14286e895819a9e8
alinemati45 <nemati@uwm.edu>
2025-04-09 20:51:10 -0500

    update code here and add new comment

--- Project 1/books.py
+++ Project 1/books.py
@@ -2,14 +2,27 @@ from fastapi import Body, FastAPI
 
 app = FastAPI()
 
-
 BOOKS = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
     {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'},
+    {'title': 'Title Seven', 'author': 'Author Six', 'category': 'fiction'},
+    {'title': 'Title Eight', 'author': 'Author Seven', 'category': 'fiction'},
+    {'title': 'Title Nine', 'author': 'Author Eight', 'category': 'biography'},
+    {'title': 'Title Ten', 'author': 'Author One', 'category': 'history'},
+    {'title': 'Title Eleven', 'author': 'Author Three', 'category': 'science'},
+    {'title': 'Title Twelve', 'author': 'Author Two', 'category': 'biography'},
+    {'title': 'Title Thirteen', 'author': 'Author Nine', 'category': 'technology'},
+    {'title': 'Title Fourteen', 'author': 'Author Ten', 'category': 'technology'},
+    {'title': 'Title Fifteen', 'author': 'Author Five', 'category': 'fiction'},
+    {'title': 'Title Sixteen', 'author': 'Author Seven', 'category': 'history'},
+    {'title': 'Title Seventeen', 'author': 'Author Four', 'category': 'fiction'},
+    {'title': 'Title Eighteen', 'author': 'Author Eight', 'category': 'science'},
+    {'title': 'Title Nineteen', 'author': 'Author Nine', 'category': 'math'},
+    {'title': 'Title Twenty', 'author': 'Author Ten', 'category': 'biography'}
 ]
 
 
@@ -27,10 +40,13 @@ async def read_book(book_title: str):
 
 @app.get("/books/")
 async def read_category_by_query(category: str):
-    books_to_return = []
+    books_to_return = [] # 
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
+        else:
+            # return 404 if no books are found
+            return {"error": "No books found in this category."}
     return books_to_return
 
 

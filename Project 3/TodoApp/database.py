470057e2861457a638f531dee742d71d751fa064
rezashakeri45 <nemati@uwm.edu>
2025-04-13 03:04:45 -0500

    update based on docker

--- Project 3/TodoApp/database.py
+++ Project 3/TodoApp/database.py
@@ -1,11 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'todo_app')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'db')  # Default to 'db', which is a common Docker container name for PostgreSQL
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'password')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
